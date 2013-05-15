7613836d7673439a460fc4897e0fc523d4c7a858
mazenovi <vmazenod@gmail.com>
2013-05-15 17:46:31 +0200

    ignore staled extra behaviors in beahvrios and export tab

---
+++ PropelTabBehaviors.py
@@ -0,0 +1,192 @@
+from PropelTabGrid import *
+from PropelBehavior import *
+from PropelForm import *
+import mforms
+import re
+import pickle
+
+__all__ = ["PropelTabBehaviors"]
+
+VERSION = "1.0.0"
+
+class PropelTabBehaviors(PropelTabGrid):
+
+  fields_list = [
+    'table',
+    'name',
+    'parameter',
+    'value'
+  ]
+
+  defaults = {
+    'extra_behaviors_path':'behaviors.py'
+  }
+
+  def __init__(self, bool, name, db):
+    self.db = db
+
+    if not self.db.wbObject.customData['extra_behaviors_path']:
+      self.db.cache['extra_behaviors_path'] = self.defaults['extra_behaviors_path']
+    else:
+      self.db.cache['extra_behaviors_path'] = self.db.wbObject.customData['extra_behaviors_path']
+
+    self.fields = PropelBehavior.fields
+    self.fields['name']['items'] = sorted(PropelBehavior.getBehaviorsDict(self.db.cache['extra_behaviors_path']).keys())
+    self.behaviors = PropelBehavior.getBehaviorsDict(self.db.cache['extra_behaviors_path'])
+    super(PropelTabBehaviors, self).__init__(bool, name)
+    self.widgets['behaviors'] = mforms.newTreeView(1)
+    self.search('behaviors')
+    self.colmuns_name('behaviors')
+    self.browse_extra_behaviors_box()
+    self.add_remove_behavior_button()    
+    self.add_end(self.widgets['behaviors'], True, True)
+    
+
+  def find_rows(self, selected_row):
+    candidates = []
+    for table in self.db.tables:
+      if self.widgets['behaviors_search_pattern'].get_string_value() == "" or re.search(self.widgets['behaviors_search_pattern'].get_string_value(), table.get_name):
+        candidates.append(table)
+    self.widgets['behaviors'].clear_rows()
+    self.widgets['behaviors_search_match_count'].set_text("%i table(s) found" % len(candidates))
+    for table in candidates:
+      row = self.widgets['behaviors'].add_row()
+      self.widgets['behaviors'].set_string(row, self.fields_list.index('table'), table.get_name)
+      for behavior in table.behaviors:
+        if self.behaviors.has_key(behavior.get_name):
+          for parameter in self.behaviors[behavior.get_name]:
+            row = self.widgets['behaviors'].add_row()
+            for lineNumber, fieldName in enumerate(self.fields_list):
+              if fieldName == 'name':
+                self.widgets['behaviors'].set_string(row, lineNumber, str(behavior.get_name))
+              elif fieldName == 'parameter':
+                self.widgets['behaviors'].set_string(row, lineNumber, str(parameter))
+              elif fieldName == 'value':
+                self.widgets['behaviors'].set_string(row, lineNumber, str(getattr(behavior, 'get_parameter_' + parameter)))
+              elif self.fields[fieldName]['type'] == mforms.StringColumnType:
+                self.widgets['behaviors'].set_string(row, lineNumber, str(getattr(behavior, 'get_' + fieldName)))
+              elif self.fields[fieldName]['type'] == mforms.CheckColumnType:
+                self.widgets['behaviors'].set_bool(row, lineNumber, int(getattr(behavior, 'get_' + fieldName)))
+        # we must add a line for behavior without any argument too
+        if self.behaviors.has_key(behavior.get_name) and len(self.behaviors[behavior.get_name]) == 0:
+          row = self.widgets['behaviors'].add_row()
+          for lineNumber, fieldName in enumerate(self.fields_list):
+            if fieldName == 'name':
+              self.widgets['behaviors'].set_string(row, lineNumber, str(behavior.get_name))
+            elif fieldName == 'parameter':
+              self.widgets['behaviors'].set_string(row, lineNumber, '')
+            elif fieldName == 'value':
+              self.widgets['behaviors'].set_string(row, lineNumber, '')
+            elif self.fields[fieldName]['type'] == mforms.StringColumnType:
+              self.widgets['behaviors'].set_string(row, lineNumber, '')
+            elif self.fields[fieldName]['type'] == mforms.CheckColumnType:
+              self.widgets['behaviors'].set_bool(row, lineNumber, '')
+    self.widgets['behaviors'].set_selected(selected_row)
+    
+
+  def activate_field(self, edited_row, edited_col):
+    fieldName = self.fields_list[edited_col]
+    if self.fields[fieldName].has_key('items') and len(self.fields[fieldName]['items']) > 0:
+      self.select_box('behaviors', edited_row, edited_col, fieldName)
+
+  def edit_field(self, edited_row, edited_col, value):
+    if value != '':
+      fieldName = self.fields_list[edited_col]
+      keys = range(0, self.widgets['behaviors'].get_selected()+1)
+      keys.reverse()
+      for i in keys:
+        if self.widgets['behaviors'].get_string(i, self.fields_list.index('table')):
+          tableName = str(self.widgets['behaviors'].get_string(i, self.fields_list.index('table')))
+          break
+      for table in self.db.tables:
+        if table.get_name == tableName:
+          t = table
+          break
+      if fieldName == 'name':
+        behaviorName = value
+        t.behaviors.append(PropelBehavior({'name':behaviorName}, t))
+      elif fieldName == 'value':
+        behaviorName = self.widgets['behaviors'].get_string(edited_row, self.fields_list.index('name'))
+        parameterName = self.widgets['behaviors'].get_string(edited_row, self.fields_list.index('parameter'))
+        for behavior in t.behaviors:
+          if behavior.get_name == behaviorName:
+            b = behavior
+            break
+        setattr(b, 'set_parameter_' + parameterName, value)
+    self.find_rows(self.widgets['behaviors'].get_selected()+1)
+
+  def add_remove_behavior_button(self):
+    tBox = PropelForm.spaced_box(True)
+    self.widgets['selectBehaviors'] = mforms.newSelector(mforms.SelectorPopup)
+    self.widgets['selectBehaviors'].add_items(sorted(PropelBehavior.getBehaviorsDict(self.widgets['extra_behaviors_path'].get_string_value()).keys()))
+    tBox.add(self.widgets['selectBehaviors'], False, True)
+    addBehavior = mforms.newButton()
+    addBehavior.set_text("add selected behavior to selected table")
+    addBehavior.add_clicked_callback(lambda: self.add_behavior())
+    tBox.add(addBehavior, False, True)
+    removeBehavior = mforms.newButton()
+    removeBehavior.set_text("remove this behavior")
+    removeBehavior.add_clicked_callback(lambda: self.remove_behavior())
+    tBox.add(removeBehavior, False, True)
+    self.add_end(tBox, False, True)
+
+  def add_behavior(self):
+    behaviorName = str(self.widgets['selectBehaviors'].get_string_value())
+    tableName = self.widgets['behaviors'].get_string(self.widgets['behaviors'].get_selected(), self.fields_list.index('table'))
+    if behaviorName and tableName:
+      for table in self.db.tables:
+        if table.get_name == tableName:
+          t = table
+          break
+      t.behaviors.append(PropelBehavior({'name':behaviorName}, t))
+      self.find_rows(self.widgets['behaviors'].get_selected()+1)
+    else:
+      mforms.Utilities.show_warning("Warning", "Please select a table row with a not null 'name' column to add a behavior", "OK", "", "")
+
+  def remove_behavior(self):
+    behaviorName = self.widgets['behaviors'].get_string(self.widgets['behaviors'].get_selected(), self.fields_list.index('name'))
+    keys = range(0, self.widgets['behaviors'].get_selected()+1)
+    keys.reverse()
+    tableIndex = 0
+    for i in keys:
+      if self.widgets['behaviors'].get_string(i, self.fields_list.index('table')):
+        tableName = self.widgets['behaviors'].get_string(i, self.fields_list.index('table'))
+        break
+    for i, table in enumerate(self.db.tables):
+        if table.get_name == tableName:
+          tableIndex = i
+          break
+    if behaviorName and tableName:
+      for i, behavior in enumerate(self.db.tables[tableIndex].behaviors):
+        if behavior.get_name == behaviorName:
+          del self.db.tables[tableIndex].behaviors[i]
+          break
+      self.find_rows(tableIndex)
+    else:
+      mforms.Utilities.show_warning("Warning", "Please select a behavior row with a not null 'behavior' column to remove this behavior", "OK", "", "")
+
+  def browse_extra_behaviors_box(self):
+    tBox = PropelForm.spaced_box(True)
+    label = mforms.newLabel("extra behaviors")
+    tBox.add(label, False, True)
+    self.widgets['extra_behaviors_path'] = mforms.newTextEntry()
+    self.widgets['extra_behaviors_path'].set_value(self.db.cache['extra_behaviors_path'])
+    tBox.add(self.widgets['extra_behaviors_path'], True, True)
+    self.widgets['extra_behaviors_file'] = mforms.newFileChooser(mforms.OpenFile)
+    self.widgets['extra_behaviors_file'].set_extensions('Python files (*.py)|*.py','py')
+    self.widgets['extra_behaviors_file'].set_title("extra behaviors file")
+    browse = mforms.newButton()
+    browse.set_text("Browse")
+    browse.add_clicked_callback(lambda: self.browse_schema())
+    tBox.add(browse, False, True)
+    label = mforms.newLabel("see http://m4z3.me/extrabehaviors for details")
+    tBox.add(label, False, True)
+    self.add_end(tBox, False, True)
+
+  def browse_schema(self):
+    self.widgets['extra_behaviors_file'].run_modal()
+    self.widgets['extra_behaviors_path'].set_value(self.widgets['extra_behaviors_file'].get_path())
+    self.db.cache['extra_behaviors_path'] = self.widgets['extra_behaviors_path'].get_string_value()
+    self.widgets['selectBehaviors'].clear()
+    self.widgets['selectBehaviors'].add_items(sorted(PropelBehavior.getBehaviorsDict(self.widgets['extra_behaviors_path'].get_string_value()).keys()))
+      
---
+++ PropelTabExport.py
@@ -0,0 +1,244 @@
+from PropelTabFile import *
+from PropelDatabase import *
+
+from PropelTable import *
+from PropelColumn import *
+from PropelForeignKey import *
+from PropelBehavior import *
+from PropelIndice import *
+from PropelExternalSchema import *
+from PropelForm import *
+import ElementTree as ET
+import mforms
+import re
+
+__all__ = ["PropelTabExport"]
+
+VERSION = "1.0.0"
+
+class PropelTabExport(PropelTabFile):
+
+  defaults = {
+    'export_FK_name':False,
+    'export_add_ai_on_pk':False,
+    'export_index':False,
+    'export_index_name':False,
+    'export_unique':False,
+    'export_unique_name':False,
+    'export_schema_path':'schema.xml',
+    'text_editor_width':970,
+    'text_editor_height':300
+  }
+
+  def __init__(self, bool, name, db):
+    self.db = db
+    for k, v in self.defaults.iteritems():
+      if not self.db.wbObject.customData.has_key(k):
+        self.db.cache[k] = v
+      else:
+        self.db.cache[k] = self.db.wbObject.customData[k]
+    self.propelBehaviorsDict = PropelBehavior.getBehaviorsDict(self.db.cache['extra_behaviors_path'])
+    super(PropelTabExport, self).__init__(bool, name)
+    self.options_schema_box()
+    self.text_editor_schema_box()
+    self.browse_schema_box()
+    self.refresh_text_editor_schema_box()
+
+  # return a xml propel schema from catalog
+  def get_xmlized_schema(self):
+    database = ET.Element('database')
+    for k, v in PropelDatabase.fields.iteritems():
+      if self.db.cache.has_key(k) and self.db.cache[k]:
+        database.attrib[k] = self.db.cache[k]
+      elif not PropelDatabase.fields[k]['optional']:
+        database.attrib[k] = PropelDatabase.fields[k]['default']
+    database = self.convert_bool_value(database, PropelDatabase)
+    for t in self.db.tables:
+      if t.get_export == '1':
+        table = ET.SubElement(database, 'table')
+        for k, v in PropelTable.fields.iteritems():
+          if getattr(t, 'get_' + k) and getattr(t, 'get_' + k) != PropelTable.fields[k]['default']:
+            table.attrib[k] = getattr(t, 'get_' + k)
+          elif not PropelTable.fields[k]['optional']:
+            table.attrib[k] = PropelTable.fields[k]['default']
+        table = self.convert_bool_value(table, PropelTable)
+        for c in t.columns:
+          column = ET.SubElement(table, 'column')
+          for k, v in PropelColumn.fields.iteritems():
+            if k != 'table':
+              if getattr(c, 'get_' + k)  and getattr(c, 'get_' + k) != PropelColumn.fields[k]['default']:
+                column.attrib[k] = str(getattr(c, 'get_' + k))
+              elif c.get_primaryKey and k == 'autoIncrement' and self.widgets['export_add_ai_on_pk'].get_bool_value():
+                column.attrib[k] = 'True'
+              elif not PropelColumn.fields[k]['optional']:
+                column.attrib[k] = PropelColumn.fields[k]['default']
+          column = self.convert_bool_value(column, PropelColumn)
+        for fk in t.foreignKeys:
+          foreign_key = ET.SubElement(table, 'foreign-key')
+          for k, v in PropelForeignKey.fields.iteritems():
+            if k != 'table' and k != 'localColumn' and k!= 'foreignColumn':
+              if k == 'name':
+                if self.widgets['export_FK_name'].get_bool_value():
+                  foreign_key.attrib[k] = str(getattr(fk, 'get_' + k))
+              elif getattr(fk, 'get_' + k) and getattr(fk, 'get_' + k) != PropelForeignKey.fields[k]['default']:
+                foreign_key.attrib[k] = str(getattr(fk, 'get_' + k))
+              elif not PropelForeignKey.fields[k]['optional']:
+                foreign_key.attrib[k] = PropelForeignKey.fields[k]['default']
+          foreign_key = self.convert_bool_value(foreign_key, PropelForeignKey)
+          for k, col in enumerate(fk.wbObject.referencedColumns):
+            reference = ET.SubElement(foreign_key, 'reference')
+            reference.attrib['local'] = str(getattr(fk, 'get_localColumn_' + str(k)))
+            reference.attrib['foreign'] = str(getattr(fk, 'get_foreignColumn_' + str(k)))
+        for i in t.indices:
+          if (i.get_indexType == 'UNIQUE' and self.widgets['export_unique'].get_bool_value()) or (i.get_indexType == 'INDEX' and self.widgets['export_index'].get_bool_value()):
+            type = i.get_indexType.lower()
+            indice = ET.SubElement(table, type)
+            for k, v in PropelIndice.fields.iteritems():
+              if k != 'columnName' and k != 'table' and k != 'indexType':
+                if k == 'name':
+                  if (i.get_indexType == 'UNIQUE' and self.widgets['export_unique_name'].get_bool_value()) or (i.get_indexType == 'INDEX' and self.widgets['export_index_name'].get_bool_value()):
+                    indice.attrib[k] = str(getattr(i, 'get_' + k))
+                elif getattr(i, 'get_' + k):
+                  indice.attrib[k] = str(getattr(i, 'get_' + k))
+                elif not PropelIndice.fields[k]['optional']:
+                  indice.attrib[k] = PropelIndice.fields[k]['default']
+            for column_name in i.get_columnsName:
+              indice_column = ET.SubElement(indice, type + '-column')
+              indice_column.attrib['name'] = column_name
+            indice = self.convert_bool_value(indice, PropelIndice)
+        for k, b in enumerate(t.behaviors):
+          if self.propelBehaviorsDict.has_key(b.get_name):
+            behavior = ET.SubElement(table, 'behavior')          
+            behavior.attrib['name'] = b.get_name          
+            for p in self.propelBehaviorsDict[b.get_name]:
+              if getattr(b, 'get_parameter_' + str(p)) != '':
+                parameter = ET.SubElement(behavior, 'parameter')
+                parameter.attrib['name'] = p
+                parameter.attrib['value'] = getattr(b, 'get_parameter_' + str(p))      
+    for k, es in enumerate(self.db.externalSchemas):
+      external_schema = ET.SubElement(database, 'external-schema')
+      for k, v in PropelExternalSchema.fields.iteritems():
+        if getattr(es, 'get_' + k):
+          external_schema.attrib[k] = str(getattr(es, 'get_' + k))
+        elif not PropelExternalSchema.fields[k]['optional']:
+          external_schema.attrib[k] = PropelExternalSchema.fields[k]['default']
+      external_schema = self.convert_bool_value(external_schema, PropelExternalSchema)
+    self.indent(database)
+    return '<?xml version="1.0" encoding="UTF-8"?>\r\n' + ET.tostring(database)
+
+  def convert_bool_value(self, xmltag, associatedClass):
+    for k, v in xmltag.attrib.iteritems():
+      if associatedClass.fields[k]['type'] == mforms.CheckColumnType:
+        if v:
+          xmltag.attrib[k] = 'true'
+        else:
+          xmltag.attrib[k] = 'false'
+    return xmltag
+
+  def indent(self, elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            self.indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+  def text_editor_schema_box(self):
+    tBox = PropelForm.spaced_box(True)
+    self.widgets['export_text_editor'] = mforms.newTextBox(mforms.BothScrollBars)
+    self.widgets['export_text_editor'].set_size(self.defaults['text_editor_width'], self.defaults['text_editor_height'])
+    self.widgets['export_text_editor'].set_bordered(True)
+    self.widgets['export_text_editor'].set_read_only(False)
+    tBox.add(self.widgets['export_text_editor'], False, True)
+    self.add(tBox, False, True)
+
+  def options_schema_box(self):
+    tBox = PropelForm.spaced_box(True)
+
+    self.widgets['export_FK_name'] = mforms.newCheckBox()
+    self.widgets['export_FK_name'].set_text('export all FK name')
+    self.widgets['export_FK_name'].set_active(self.db.cache['export_FK_name'])
+    self.widgets['export_FK_name'].add_clicked_callback(lambda: self.refresh_text_editor_schema_box())
+    tBox.add(self.widgets['export_FK_name'], False, True)
+
+    self.widgets['export_add_ai_on_pk'] = mforms.newCheckBox()
+    self.widgets['export_add_ai_on_pk'].set_text('add AI to PK')
+    self.widgets['export_add_ai_on_pk'].set_active(self.db.cache['export_add_ai_on_pk'])
+    self.widgets['export_add_ai_on_pk'].add_clicked_callback(lambda: self.refresh_text_editor_schema_box())
+    tBox.add(self.widgets['export_add_ai_on_pk'], False, True)
+
+    self.widgets['export_index'] = mforms.newCheckBox()
+    self.widgets['export_index'].set_text('export all indexes')
+    self.widgets['export_index'].set_active(self.db.cache['export_index'])
+    self.widgets['export_index'].add_clicked_callback(lambda: self.refresh_text_editor_schema_box())
+    tBox.add(self.widgets['export_index'], False, True)
+
+    self.widgets['export_index_name'] = mforms.newCheckBox()
+    self.widgets['export_index_name'].set_text('export all indexe\'s name')
+    self.widgets['export_index_name'].set_active(self.db.cache['export_index_name'])
+    self.widgets['export_index_name'].add_clicked_callback(lambda: self.refresh_text_editor_schema_box())
+    tBox.add(self.widgets['export_index_name'], False, True)
+
+    self.widgets['export_unique'] = mforms.newCheckBox()
+    self.widgets['export_unique'].set_text('export all uniques')
+    self.widgets['export_unique'].set_active(self.db.cache['export_unique'])
+    self.widgets['export_unique'].add_clicked_callback(lambda: self.refresh_text_editor_schema_box())
+    tBox.add(self.widgets['export_unique'], False, True)
+
+    self.widgets['export_unique_name'] = mforms.newCheckBox()
+    self.widgets['export_unique_name'].set_text('export all unique\'s name')
+    self.widgets['export_unique_name'].set_active(self.db.cache['export_unique_name'])
+    self.widgets['export_unique_name'].add_clicked_callback(lambda: self.refresh_text_editor_schema_box())
+    tBox.add(self.widgets['export_unique_name'], False, True)
+
+    self.add(tBox, False, True)
+
+  def refresh_text_editor_schema_box(self):
+    self.db.cache['export_FK_name'] = self.widgets['export_FK_name'].get_bool_value()
+    self.db.cache['export_add_ai_on_pk'] = self.widgets['export_add_ai_on_pk'].get_bool_value()
+    self.db.cache['export_index'] = self.widgets['export_index'].get_bool_value()
+    self.db.cache['export_index_name'] = self.widgets['export_index_name'].get_bool_value()
+    self.db.cache['export_unique'] = self.widgets['export_unique'].get_bool_value()
+    self.db.cache['export_unique_name'] = self.widgets['export_unique_name'].get_bool_value()
+    self.widgets['export_text_editor'].set_value(self.get_xmlized_schema())
+
+  def browse_schema_box(self):
+    tBox = PropelForm.spaced_box(True)
+    label = mforms.newLabel("export propel schema")
+    tBox.add(label, False, True)
+    self.widgets['export_schema_path'] = mforms.newTextEntry()
+    self.widgets['export_schema_path'].set_value(self.db.cache['export_schema_path'])
+    tBox.add(self.widgets['export_schema_path'], True, True)
+    self.widgets['export_schema_file'] = mforms.newFileChooser(mforms.SaveFile)
+    self.widgets['export_schema_file'].set_extensions('XML files (*.xml)|*.xml','xml')
+    self.widgets['export_schema_file'].set_title("export schema file")
+    browse = mforms.newButton()
+    browse.set_text("Browse")
+    browse.add_clicked_callback(lambda: self.browse_schema())
+    tBox.add(browse, False, True)
+    save = mforms.newButton()
+    save.set_text("Save")
+    save.add_clicked_callback(lambda: self.save_schema())
+    tBox.add(save, False, True)
+    self.add(tBox, False, True)
+
+  def browse_schema(self):
+    self.widgets['export_schema_file'].run_modal()
+    self.widgets['export_schema_path'].set_value(self.widgets['export_schema_file'].get_path())
+    self.db.cache['export_schema_path'] = self.widgets['export_schema_path'].get_string_value()
+
+  def save_schema(self):
+    if self.widgets['export_schema_path'].get_string_value() != "":
+      f = open(self.widgets['export_schema_path'].get_string_value(),"w")
+      f.write(self.get_xmlized_schema())
+      f.close()
+    else:
+      mforms.Utilities.show_warning("Warning", "Please select a xml file to export schema", "OK", "", "")
+
